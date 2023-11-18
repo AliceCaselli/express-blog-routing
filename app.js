8799ca83dfefe23fbfb15bd3daa61eeb43887a4a
AliceCaselli <alicecaselli3@gmail.com>
2023-11-18 12:48:51 +0100

    esercizio base completo

--- app.js
+++ app.js
@@ -1,10 +1,3 @@
-// Usando l’array dei post fornito con le relative immagini, creare un file di routing (routers/posts.js) che conterrà le rotte necessario per l’entità post.
-// All’interno creare le seguenti rotte:
-// /:slug - show: tramite il parametro dinamico che rappresenta lo slug del post, ritornerà un json con i dati del post
-// /create - create: ritornerà un semplice html con un h1 con scritto Creazione nuovo post e nel caso venga richiesta una risposta diversa da html lancerà un errore 406
-// /:slug/download - download: dovrà far scaricare l’immagine del post rappresentato dallo slug. Attenzione, se lo slug contiene il simbolo / la rotta non funzionerà. C’è qualche strumento che ci permette di codificare lo slug?
-// Scrivere tutte le funzioni delle rotte nel controller dedicato
-// Registrare il router dentro app.js con il prefisso posts/.
 // Bonus
 // Nella rotta show, aggiungere al post una proprietà image_url dove creare il link completo per l’immagine
 // Aggiungere un altra proprietà image_download_url che invece dovrà far scaricare l’immagine puntando alla rotta download
--- controllers/posts.js
+++ controllers/posts.js
@@ -1,7 +1,10 @@
 const posts = require("../db.js");
+const path = require("path");
 
 // Creiamo poi una rotta /posts che ritorni tramite content negotiation la lista dei post, da un array locale. Ritorniamo i dati sotto forma di json e html stampando una semplice ul.
 function index(req, res) {
+
+    // console.log("ciao da index start");
     res.format({
         html: () => {
             const html = ["<h1> Elenco posts </h1>"]
@@ -19,6 +22,8 @@ function index(req, res) {
             html.push("</ul>");
 
             res.send(html.join(""));
+            // console.log("ciao da index");
+
         },
 
         json: () => {
@@ -30,11 +35,15 @@ function index(req, res) {
 
         default: () => {
             res.status(406).send("Error");
+            // console.log("ciao da index default");
+
         }
     });
 }
 
+// /:slug - show: tramite il parametro dinamico che rappresenta lo slug del post, ritornerà un json con i dati del post
 function show(req, res) {
+    // console.log("ciao dalla show start");
 
     res.format({
         json: () => {
@@ -48,10 +57,14 @@ function show(req, res) {
             }
 
             res.json(post);
+            // console.log("ciao dalla show");
+
         },
 
         default: () => {
-            res.status(404).send("no buono");
+            res.status(404).send("Error");
+            // console.log("ciao dalla show default");
+
         }
 
 
@@ -59,9 +72,41 @@ function show(req, res) {
 
 }
 
+// /create - create: ritornerà un semplice html con un h1 con scritto Creazione nuovo post e nel caso venga richiesta una risposta diversa da html lancerà un errore 406
+function create(req, res) {
 
+    // console.log("ciao dalla create start");
+    res.format({
+        html: () => {
+            const html = "<h1>Creazione nuovo post</h1>";
 
+            res.send(html);
+            // console.log("ciao dalla create");
+        },
 
+        default: () => {
+
+            res.status(406).send("Error");
+            // console.log("ciao dalla create default");
+
+        }
+    })
+}
+
+// /:slug/download - download: dovrà far scaricare l’immagine del post rappresentato dallo slug. Attenzione, se lo slug contiene il simbolo / la rotta non funzionerà. C’è qualche strumento che ci permette di codificare lo slug?
+function download(req, res) {
+
+    const postSlug = req.params.slug;
+    const post = posts.find((post) => post.slug == postSlug);
+
+    if (!post) {
+        res.status(404).send("Error");
+        return;
+    }
+
+    const imagePath = path.join(__dirname, "../public/imgs/posts", post.image);
+    res.download(imagePath, post.image);
+}
 
 
 
@@ -74,4 +119,6 @@ function show(req, res) {
 module.exports = {
     index,
     show,
+    create,
+    download,
 }
